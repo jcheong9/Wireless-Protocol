f8f25be4d8aa1232bcef817f581d113d873d8526
Sammy <bigsaem@gmail.com>
2019-11-24 11:51:04 -0800

    packetize stream

---
+++ Wireless Protocol/DataLink.cpp
@@ -0,0 +1,6 @@
+#include "DataLink.h"
+
+int packetizeFile(string filePath) {
+	dataLink->uploadedFrames = new vector<char[1024]>();
+
+}
---
+++ Wireless Protocol/DataLink.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <windows.h>
+#include <string>
+#include <vector>
+#define DWORD_SIZE 1017;
+using namespace std;
+int packetizeFile(string filePath);
+struct DataLink {
+	vector<char [1024]>* uploadedFrames;
+};
+
+extern DataLink* dataLink;
