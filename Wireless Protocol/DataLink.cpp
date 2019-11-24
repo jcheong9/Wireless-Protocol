a29f255ac8b5e78af18b0c46aad7ea079907ffcf
Sammy <bigsaem@gmail.com>
2019-11-24 12:13:49 -0800

    error fixed

--- Wireless Protocol/DataLink.cpp
+++ Wireless Protocol/DataLink.cpp
@@ -1,6 +1,14 @@
 #include "DataLink.h"
-
+DataLink* dataLink = new DataLink();
 int packetizeFile(string filePath) {
 	dataLink->uploadedFrames = new vector<char[1024]>();
+	ifstream file{ filePath };
+	char c;
+	int counter = 0;
+	while (file.get(c)) {
+	/*	if (DWORD_SIZE == 0) {
 
+		}*/
+	}
+	return 1;
 }
