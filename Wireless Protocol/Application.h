eb7410e5c539cab6c1f40e979d21a4bf4d80dd09
Jameson Cheong <jctheathlete@gmail.com>
2019-11-28 09:36:10 -0800

    Update Application.h
    added the modes

--- Wireless Protocol/Application.h
+++ Wireless Protocol/Application.h
@@ -27,4 +27,12 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam);
 void setMenuButton(HWND hwnd, UINT uIDEnableItem, UINT uEnable);
 void printToWindow(HWND hwnd, HDC hdc, char* str, unsigned int* x, unsigned int* y);
 
+
+#define IDLE			50
+#define RECEIVE_MODE		51
+#define SEND_MODE		52
+
+
+
+
 #endif
